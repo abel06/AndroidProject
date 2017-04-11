3f46aa363a5f53fa964f94aa3f8f63888c85d6e6
Marco Anastasio <Marco_Anastasio@libero.it>
2017-04-11 11:18:59 +0200

    Login have been finished (Tested a bit)

--- app/src/main/java/com/example/abela/marketspiral/Login.java
+++ app/src/main/java/com/example/abela/marketspiral/Login.java
@@ -2,9 +2,9 @@ package com.example.abela.marketspiral;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.design.widget.TextInputEditText;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
-import android.widget.EditText;
 import com.example.abela.marketspiral.interfaces.LoginResponse;
 
 import java.util.HashMap;
@@ -28,17 +28,17 @@ public class Login extends AppCompatActivity implements LoginResponse{
 
         }else {
 
-           //TODO Display error message
+           System.out.println(output);
         }
     }
 
     public void login(View view) {
         HashMap<String, String> user_data = new HashMap<>();
 
-        user_data.put("username",((EditText)view.findViewById(R.id.input_username)).getText().toString());
-        user_data.put("password",((EditText)view.findViewById(R.id.input_password)).getText().toString());
-
+        user_data.put("username",((TextInputEditText)findViewById(R.id.input_username)).getText().toString());
+        user_data.put("password",((TextInputEditText)findViewById(R.id.input_password)).getText().toString());
         new LoginBackFetch(this).execute(user_data);
+
 
     }
 }
