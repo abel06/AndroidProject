c45e8604d18eb30d9b870eb6fccb085e7f78c20f
Marco Anastasio <Marco_Anastasio@libero.it>
2017-04-11 10:26:47 +0200

    Implemented login function (onClick)

--- app/src/main/java/com/example/abela/marketspiral/Login.java
+++ app/src/main/java/com/example/abela/marketspiral/Login.java
@@ -3,7 +3,8 @@ package com.example.abela.marketspiral;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-
+import android.view.View;
+import android.widget.EditText;
 import com.example.abela.marketspiral.interfaces.LoginResponse;
 
 import java.util.HashMap;
@@ -18,16 +19,26 @@ public class Login extends AppCompatActivity implements LoginResponse{
 
 
 
-    void login(HashMap<String, String> user_data) {
-
-        new LoginBackFetch().execute(user_data);
-
-    }
-
-
     @Override
     public void loginFinished(String output) {
-        Intent main_activity = new Intent(this,MainActivity.class);
-        startActivity(main_activity);
+
+        if( output.equals("VALIDO")) {
+            Intent main_activity = new Intent(this, MainActivity.class);
+            startActivity(main_activity);
+
+        }else {
+
+           //TODO Display error message
+        }
+    }
+
+    public void login(View view) {
+        HashMap<String, String> user_data = new HashMap<>();
+
+        user_data.put("username",((EditText)view.findViewById(R.id.input_username)).getText().toString());
+        user_data.put("password",((EditText)view.findViewById(R.id.input_password)).getText().toString());
+
+        new LoginBackFetch(this).execute(user_data);
+
     }
 }
