ac4723bc407a6b9b269ca469ead8b7bb5142b3ef
Marco Anastasio <Marco_Anastasio@libero.it>
2017-04-19 10:14:13 +0200

    Changed the Login class to fit the new structure based on frames.

--- app/src/main/java/com/example/abela/marketspiral/Login.java
+++ app/src/main/java/com/example/abela/marketspiral/Login.java
@@ -2,22 +2,49 @@ package com.example.abela.marketspiral;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.support.design.widget.TextInputEditText;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.Button;
+
+import com.example.abela.marketspiral.Core.LoginFragment;
+import com.example.abela.marketspiral.Core.RegisterFragment;
 import com.example.abela.marketspiral.interfaces.LoginResponse;
 
-import java.util.HashMap;
-
 public class Login extends AppCompatActivity implements LoginResponse{
+    private LoginFragment login_fragment;
+    private RegisterFragment register_fragment;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.login_activity);
+        login_fragment = new LoginFragment();
+        getSupportFragmentManager().beginTransaction().add(R.id.fragment_container,login_fragment).commit();
     }
 
 
+    /***
+     * for better coding manners i moved the entire login on the activity, once the signup button or the login button are pressed the corrisponding method is called
+    **/
+    @Override
+    protected void onStart() {
+        super.onStart();
+
+        ((Button)login_fragment.getLogin_button()).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                login();
+            }
+        });
+
+
+        ((Button)login_fragment.getSign_up_button()).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                swap_to_register();
+            }
+        });
+    }
 
     @Override
     public void loginFinished(String output) {
@@ -28,17 +55,23 @@ public class Login extends AppCompatActivity implements LoginResponse{
 
         }else {
 
-           System.out.println(output);
+            System.out.println(output);
         }
     }
 
-    public void login(View view) {
-        HashMap<String, String> user_data = new HashMap<>();
 
-        user_data.put("username",((TextInputEditText)findViewById(R.id.input_username)).getText().toString());
-        user_data.put("password",((TextInputEditText)findViewById(R.id.input_password)).getText().toString());
-        new LoginBackFetch(this).execute(user_data);
+    /**
+     * This code the login part
+     * */
+    public void login() {
+        new LoginBackFetch(this).execute(login_fragment.collect_data());
+    }
 
+
+    /**Thid method swaps between login frame to register frame*/
+    public void swap_to_register() {
+        register_fragment = new RegisterFragment();
+        getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,register_fragment).commit();
 
     }
 }
