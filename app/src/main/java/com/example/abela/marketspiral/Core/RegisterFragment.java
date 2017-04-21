9c7deae208dc2799268fe0fa88c8db9d580df97f
Marco Anastasio <Marco_Anastasio@libero.it>
2017-04-21 13:53:28 +0200

    changed the structure to fit the fragmentlife cycle

--- app/src/main/java/com/example/abela/marketspiral/Core/RegisterFragment.java
+++ app/src/main/java/com/example/abela/marketspiral/Core/RegisterFragment.java
@@ -1,24 +1,30 @@
 package com.example.abela.marketspiral.Core;
 
+import android.app.Fragment;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+
+import com.example.abela.marketspiral.Login;
 import com.example.abela.marketspiral.R;
-import android.support.v4.app.Fragment;
 
 /**
  * Created by HaZe on 4/18/17.
- *  This class represents the register frame within it's elements
+ *  This class represents the register frame within it's elemexnts
  * */
 
 public class RegisterFragment extends Fragment {
 
 
+    public RegisterFragment(Login login) {
+
+    }
+
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.register_fragment_layout,container,false);
+        return inflater.inflate(R.layout.activity_signup,container,false);
     }
 
 }
