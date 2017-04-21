4c7840fbf95dcc64df27e79332197e362ca7cbff
Marco Anastasio <Marco_Anastasio@libero.it>
2017-04-21 13:53:20 +0200

    changed the structure to fit the fragmentlife cycle

--- app/src/main/java/com/example/abela/marketspiral/Core/LoginFragment.java
+++ app/src/main/java/com/example/abela/marketspiral/Core/LoginFragment.java
@@ -1,13 +1,15 @@
 package com.example.abela.marketspiral.Core;
 
+import android.app.Fragment;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.design.widget.TextInputEditText;
-import android.support.v4.app.Fragment;
+import android.support.v7.app.AppCompatActivity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import com.example.abela.marketspiral.Login;
 import com.example.abela.marketspiral.R;
 import java.util.HashMap;
 
@@ -18,9 +20,16 @@ import java.util.HashMap;
 
 public class LoginFragment extends Fragment {
 
+
+    //This is needed to prevent the fragment swap to work
+    private AppCompatActivity login;
     private View view;
     private Button sign_up_button;
     private Button login_button;
+
+    public LoginFragment(Login login) {
+        this.login = login;
+    }
 
     @Nullable
     @Override
@@ -29,17 +38,41 @@ public class LoginFragment extends Fragment {
         return view;
     }
 
-
-
-    /**
-     * Once the view have been inflated i collect buttons reference
-     * */
+    /**        This overhead is needed because it's caused on how fragment lifecycle works...    **/
     @Override
-    public void onStart() {
-        super.onStart();
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
         sign_up_button = (Button) view.findViewById(R.id.signup);
         login_button = (Button) view.findViewById(R.id.btn_login);
+
+
+        sign_up_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                ((Login) login).swapToRegister();
+            }
+        });
+
+
+        login_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                ((Login)login).login();
+            }
+        });
+
+
+
     }
+
+//    /**
+//     * Once the view have been inflated i collect buttons reference
+//     * */
+//        @Override
+//    public void onStart() {
+//        super.onStart();
+//
+//    }
 
     public Button getSign_up_button(){
         return sign_up_button;
@@ -52,7 +85,7 @@ public class LoginFragment extends Fragment {
 
     /**
      * This functions works as before, collect data from the layout
-     *  (this will be called from the Login activity
+     *  (this will be called from the Login activity)
      * **/
     public HashMap<String,String> collect_data(){
         HashMap<String,String> data = new HashMap<>();
