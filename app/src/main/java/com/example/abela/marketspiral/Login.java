f8b231e08284a0397f41a5567c7d2916ddedce7a
Marco Anastasio <Marco_Anastasio@libero.it>
2017-04-21 13:53:00 +0200

    fixed some errors when swapping between fragments

--- app/src/main/java/com/example/abela/marketspiral/Login.java
+++ app/src/main/java/com/example/abela/marketspiral/Login.java
@@ -3,13 +3,11 @@ package com.example.abela.marketspiral;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.util.Log;
-import android.view.View;
-import android.widget.Button;
-
 import com.example.abela.marketspiral.Core.LoginFragment;
 import com.example.abela.marketspiral.Core.RegisterFragment;
 import com.example.abela.marketspiral.interfaces.LoginResponse;
+
+import static com.google.android.gms.common.internal.safeparcel.SafeParcelable.NULL;
 
 public class Login extends AppCompatActivity implements LoginResponse{
     private LoginFragment login_fragment;
@@ -19,33 +17,11 @@ public class Login extends AppCompatActivity implements LoginResponse{
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.login_activity);
-        login_fragment = new LoginFragment();
-        getSupportFragmentManager().beginTransaction().add(R.id.fragment_container,login_fragment).commit();
+        login_fragment = new LoginFragment(this);
+        register_fragment = new RegisterFragment(this);
+        getFragmentManager().beginTransaction().add(R.id.fragment_container,login_fragment).commit();
     }
 
-
-    /***
-     * for better coding manners i moved the entire login on the activity, once the signup button or the login button are pressed the corresponding method is called
-    **/
-    @Override
-    protected void onStart() {
-        super.onStart();
-
-        ((Button)login_fragment.getLogin_button()).setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                login();
-            }
-        });
-
-
-        ((Button)login_fragment.getSign_up_button()).setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                swapToRegister();
-            }
-        });
-    }
 
     @Override
     public void loginFinished(String output) {
@@ -60,19 +36,30 @@ public class Login extends AppCompatActivity implements LoginResponse{
         }
     }
 
-
     /**
      * This code the login part
      * */
     public void login() {
-        new LoginBackFetch(this).execute(login_fragment.collect_data());
+//        new LoginBackFetch(this).execute(login_fragment.collect_data());
     }
 
 
     /**Thid method swaps between login frame to register frame*/
     public void swapToRegister() {
-        register_fragment = new RegisterFragment();
-        getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,register_fragment).commit();
+        getFragmentManager().beginTransaction().replace(R.id.fragment_container,register_fragment).addToBackStack(NULL).commit();
+
+    }
+
+
+    @Override
+    public void onBackPressed() {
+        System.out.println(getFragmentManager().getBackStackEntryCount());
+        if(getFragmentManager().getBackStackEntryCount() == 1) {
+            getFragmentManager().popBackStack();
+
+        }else{
+            super.onBackPressed();
+        }
 
     }
 }
