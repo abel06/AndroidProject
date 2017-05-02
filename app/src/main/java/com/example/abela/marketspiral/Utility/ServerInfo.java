546ab3f1427ba7f745b554e20959e82271ce29f9
Marco Anastasio <Marco_Anastasio@libero.it>
2017-05-02 14:36:01 +0200

    Added the ServerInfo class

---
+++ app/src/main/java/com/example/abela/marketspiral/Utility/ServerInfo.java
@@ -0,0 +1,15 @@
+package com.example.abela.marketspiral.Utility;
+
+/**
+ * Created by HaZe on 5/2/17.
+ * This class stores the information about server
+ */
+public final class ServerInfo {
+
+    public static final String DB_URL = "http://10.0.2.2/" ;
+    public static final String REGISTER = "register.php";
+    public static final String LOGIN = "login.php";
+    public static final String NEW_ITEM = "new_item.php";
+    public static final String REMOVE_ITEM = "remove_item.php";
+
+}
